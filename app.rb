65d8a416ee371aa43401e454914335f3dc73014d
nlaz <nikolazaris@utexas.edu>
2016-03-13 19:45:10 -0500

    add client

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'omniauth-twitter'
 require 'httparty'
 require 'json'
+require 'twitter'
+require 'pry'
+require 'tilt/erubis'
 
 require 'dotenv'
 Dotenv.load
@@ -12,7 +15,7 @@ configure do
   enable :sessions
 
   use OmniAuth::Builder do
-    provider :twitter, ENV['TWITTER_CLIENT_KEY'], ENV['TWITTER_CLIENT_SECRET']
+    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   end
 end
 
@@ -21,6 +24,15 @@ end
 helpers do
   def current_user
     !session[:uid].nil?
+  end
+
+  def client
+    client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token        = session[:token]
+      config.access_token_secret = session[:secret]
+    end
   end
 end
 
@@ -32,11 +44,16 @@ end
 # Routes
 
 get '/' do
+  screen_names = client.friends.collect {|f| p client.user(f).screen_name }
+
   erb :index
 end
 
 get '/auth/twitter/callback' do
-  session[:uid] = env['omniauth.auth']['uid']
+  session[:uid]    = env['omniauth.auth']['uid']
+  session[:token]  = env['omniauth.auth']['extra']['access_token'].token
+  session[:secret] = env['omniauth.auth']['extra']['access_token'].secret
+
   redirect to('/')
 end
 
