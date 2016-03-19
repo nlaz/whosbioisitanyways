8b3f37cb2cbdb65ad14abfd8d75f34c1d9288607
nlaz <nikolazaris@utexas.edu>
2016-03-19 00:46:39 -0500

    create global variables

--- app.rb
+++ app.rb
@@ -9,70 +9,79 @@ require 'tilt/erubis'
 require 'dotenv'
 Dotenv.load
 
-# Configuration
+class WhoseBioApp < Sinatra::Base
 
-configure do
-  enable :sessions
+  # Configuration
 
-  use OmniAuth::Builder do
-    provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
-  end
-end
+  configure do
+    enable :sessions
 
-# Helpers
-
-helpers do
-  def current_user
-    !session[:uid].nil?
-  end
-
-  def twitter_client
-    client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = session[:token]
-      config.access_token_secret = session[:secret]
+    use OmniAuth::Builder do
+      provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
     end
   end
 
-  def fetch_all_friends
-    slice_size = 100
-    friends = []
-    twitter_client.friend_ids.each_slice(slice_size).with_index do |slice, i|
-      twitter_client.users(slice).each do |friend|
-        friends << friend
+  # Helpers
+
+  helpers do
+    def current_user
+      !session[:uid].nil?
+    end
+
+    def twitter_client
+      $client ||= Twitter::REST::Client.new do |config|
+        p "CREATING NEW CLIENT"
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token        = session[:token]
+        config.access_token_secret = session[:secret]
       end
     end
-    friends
+
+    def friends_list
+      $friends ||= fetch_all_friends
+    end
+
+    def fetch_all_friends
+      slice_size = 100
+      friends = []
+      twitter_client.friend_ids.each_slice(slice_size).with_index do |slice, i|
+        twitter_client.users(slice).each do |friend|
+          p "SHOULDNT BE HERE EVERY TIME!"
+          friends << friend
+        end
+      end
+      friends
+    end
   end
-end
 
-before do
-  pass if request.path_info =~ /^\/auth\//
-  redirect to('/auth/login') unless current_user
-end
+  before do
+    pass if request.path_info =~ /^\/auth\//
+    redirect to('/auth/login') unless current_user
+  end
 
-# Routes
+  # Routes
 
-get '/' do
-  friends = fetch_all_friends
-  p friends
+  get '/' do
+    friends = friends_list
+    p friends.sample
 
-  erb :index
-end
+    erb :index
+  end
 
-get '/auth/twitter/callback' do
-  session[:uid]    = env['omniauth.auth']['uid']
-  session[:token]  = env['omniauth.auth']['extra']['access_token'].token
-  session[:secret] = env['omniauth.auth']['extra']['access_token'].secret
+  get '/auth/twitter/callback' do
+    session[:uid]    = env['omniauth.auth']['uid']
+    session[:token]  = env['omniauth.auth']['extra']['access_token'].token
+    session[:secret] = env['omniauth.auth']['extra']['access_token'].secret
 
-  redirect to('/')
-end
+    redirect to('/')
+  end
 
-get '/auth/failure' do
-  # TODO: Create Failure Page
-end
+  get '/auth/failure' do
+    # TODO: Create Failure Page
+  end
 
-get '/auth/login' do
-  erb :login
+  get '/auth/login' do
+    erb :login
+  end
 end
