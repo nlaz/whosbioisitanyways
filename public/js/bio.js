82f7a63384037c158ab79b80af4fb1cfedff261e
nlaz <nikolazaris@utexas.edu>
2016-03-30 21:11:33 -0700

    jquery -> javascript

--- public/js/bio.js
+++ public/js/bio.js
@@ -1,12 +1,17 @@
-$("#input").keyup(function (e) {
-  if (e.keyCode == 13) {
-    var answer = $("#input").val();
+var inputForm = document.getElementById("input");
 
-    $.get(
-      "/friend.json",
-      function(result) {
-        var user = JSON.parse(result);
-        console.log(user);
+inputForm.onkeyup = function(e) {
+  e = e || window.event;
+
+  if (e.keyCode == 13) {
+    var answer = inputForm.value;
+
+    var request = new XMLHttpRequest();
+    request.open('GET', '/friend.json', true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var user = JSON.parse(request.responseText);
         if (answer === user.name || answer === user.screen_name) {
           success(user);
         } else {
@@ -14,26 +19,32 @@ $("#input").keyup(function (e) {
         }
         revealReset(user);
       }
-    );
+    };
+
+    request.send();
     e.preventDefault();
   }
-});
+};
 
 function success(friend) {
-  $("#title").text("You got it!").css("color", "#55acee");
+  var title = document.getElementById("title");
+  title.innerHTML = "Correct! It's " + friend.name + "!";
+  title.style.color = "#55acee";
 }
 
 function failure(friend) {
-  $("#title").text("Nope! You got it wrong! It's " + friend.name + "!").css("color", "#a31400");
+  var title = document.getElementById("title");
+  title.innerHTML = "Wrong! It's actually " + friend.name + "!";
+  title.style.color = "#a31400";
 }
 
 function revealReset(friend) {
-  $("#profile").attr("src", friend.profile_image_url.replace("_normal", ""));
+  document.getElementById("profile").src = friend.profile_image_url.replace("_normal", "");
   resetForm(3000);
 }
 
 function resetForm(time) {
-  $("#countdown").text(time/1000);
+  document.getElementById("countdown").innerHTML = time/1000;
   var timeLeft = time - 1000;
   setTimeout(function () { 
     if (time <= 0){
